c3d2c424b41ecdd9d69483a35e478e921de5b30a
lukasz kryst <lukaszkryst@gmail.com>
2018-03-05 15:59:41 +0100

    Add for loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,22 @@
+for (let i=0; i<1; i++) {
+
+    console.log('    *');
+
+    for (let y=0; y<1; y++) {
+        console.log('   ***');
+    }
+    for (let y=0; y<1; y++) {
+        console.log('  *****');
+    }
+    for (let y=0; y<1; y++) {
+        console.log(' *******');
+    }
+    for (let y=0; y<1; y++) {
+        console.log('*********');
+    }
+
+}
+
 /* let star = '';
 
     for (let j=0; j<1; j++) {
